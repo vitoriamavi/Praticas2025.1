2c7baa5824e734669f70d9d39cb0503361fcb859
vitoriamavi <vitoriasilvaxxs@gmail.com>
2025-06-21 18:51:33 -0300

    21.06

--- Lista de Charles/q3.py
+++ Lista de Charles/q3.py
@@ -1,3 +1,6 @@
 '''Questão 03 (05 pontos)
 Na definição da Wikipédia, números triangulares são aqueles que representam o total de pontos que formam triângulos equiláteros em um plano.
 Faça um programa que solicite ao usuário um número inteiro positivo e informe se ele é (ou não) triangular, de acordo com a definição.'''
+
+num = int(input("Digite um número: "))
+
