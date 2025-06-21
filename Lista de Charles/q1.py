f2f1517b2b0f85232165e0c812c53bc848d58fa3
vitoriamavi <vitoriasilvaxxs@gmail.com>
2025-06-21 15:06:39 -0300

    21.06

--- Lista de Charles/q1.py
+++ Lista de Charles/q1.py
@@ -1,10 +1,20 @@
-limite = 1000000
-i = 0
+'''Questão 01 (05 pontos)
+Um determinado material radioativo perde metade de sua massa a cada 50 segundos. Faça um programa que solicite a massa inicial (em gramas) e que calcule o tempo necessário para que essa massa se torne menor que 0,5 grama. Ao término, o programa deve exibir a massa inicial, a massa final e o tempo que levou para o decaimento (exiba o tempo informando horas, minutos e segundos).
+Exemplo de saída:
+Massa Inicial: 250 gramas
+Massa Final: 0.48828125 gramas
+Tempo de Decaimento: 0:07:30'''
 
-while i < limite:
-    if i % 2 == 0 or i % 5 == 0:
-        soma = 0
-        for digito in str(i):
-            soma += digito **5
-        if soma == i:
-            print (i)
+massaInicial = float(input("Digie a massa inicial do material radioativo: "))
+tempoEmSegundos = 0
+
+while massaInicial >=  0.5:
+    massaInicial /= 2
+
+horas = tempoEmSegundos//3600
+minutos = (tempoEmSegundos % 3600) //60
+segundos = tempoEmSegundos % 60
+
+print(f"Massa inicial: {massaInicial} gramas.")
+print(f"Massa Final: {massa} gramas")
+print(f"Tempo de Decaimento: {int(horas):02d}:{int(minutos):02d}:{int(segundos):02d}")
---
+++ Lista de Charles/q2.py
@@ -0,0 +1,10 @@
+limite = 1000000
+i = 0
+
+while i < limite:
+    if i % 2 == 0 or i % 5 == 0:
+        soma = 0
+        for digito in str(i):
+            soma += digito **5
+        if soma == i:
+            print (i)
