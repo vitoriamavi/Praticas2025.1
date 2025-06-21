72d5be7c98c03b18ba2e120987d9c9c6f331e9dd
vitoriamavi <vitoriasilvaxxs@gmail.com>
2025-06-21 15:25:50 -0300

    21.06

--- Lista de Charles/q2.py
+++ Lista de Charles/q2.py
@@ -1,10 +1,30 @@
-limite = 1000000
-i = 0
+'''Existem somente quatro números, maiores do que um, que podem ser obtidos pela soma de potências de 4 dos seus dígitos:
+1643 = 1⁴ + 6⁴ + 4⁴ + 3⁴
+8208 = 8⁴ + 2⁴ + 0⁴ + 8⁴
+9474 = 9⁴ + 4⁴ + 7⁴ + 4⁴
+Faça um programa que encontra e exibe os números menores de 1.000.000, que são múltiplos de 2 ou 5 e que podem ser escritos pela soma das potências de 5 de seus dígitos.'''
 
-while i < limite:
-    if i % 2 == 0 or i % 5 == 0:
+for numero in range(2, 1000000):
+    if numero % 2 == 0 or numero % 5 == 0:
         soma = 0
-        for digito in str(i):
-            soma += digito **5
-        if soma == i:
-            print (i)
+        temp = numero
+        explicacao = ""
+        contador = 0
+
+        while temp > 0:
+            digito = temp % 10
+            potencia = digito ** 5
+            soma += potencia
+            termo = str(digito) + "^5 = " + str(potencia)
+            if contador == 0:
+                explicacao = termo
+            else:
+                explicacao = termo + " + " + explicacao
+            temp //= 10
+            contador += 1
+
+        if soma == numero:
+            print("Número encontrado: " + str(numero))
+            print("Componentes da soma:")
+            print(explicacao)
+            print("-" * 40)
