828ffedf3e109d41018ef36eb22ca4a1377ad6e4
vitoriamavi <vitoriasilvaxxs@gmail.com>
2025-06-22 18:10:11 -0300

    22.06

--- Lista de Charles/q3.py
+++ Lista de Charles/q3.py
@@ -2,8 +2,23 @@
 Na definição da Wikipédia, números triangulares são aqueles que representam o total de pontos que formam triângulos equiláteros em um plano.
 Faça um programa que solicite ao usuário um número inteiro positivo e informe se ele é (ou não) triangular, de acordo com a definição.'''
 
-n = int(input("Digite um número: "))
+try:
+    num = int(input("Digite um número inteiro positivo: "))
+    if num <= 0:
+        print("Por favor, insira um número positivo.")
+    else:
+        n = 1
+        triangular = False
 
-if n > 0:
-    n**(n+1)/2 == int 
-    print("eh um número triangular")
+        while n * (n + 1) // 2 <= num:
+            if n * (n + 1) // 2 == num:
+                triangular = True
+                break
+            n += 1
+
+        if triangular:
+            print(f"O número {num} é triangular.")
+        else:
+            print(f"O número {num} não é triangular.")
+except ValueError:
+    print("Entrada inválida. Por favor, digite um número inteiro.")
