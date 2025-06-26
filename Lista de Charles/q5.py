ba3a19b8d749f63a534e5d72b068b8547953bbea
vitoriamavi <vitoriasilvaxxs@gmail.com>
2025-06-26 01:11:24 -0300

    terminando questão 5

--- Lista de Charles/q5.py
+++ Lista de Charles/q5.py
@@ -10,26 +10,24 @@ while verifica == 0:
     string1 = (input("Digite a primeira string composta apenas por 0 e 1: "))
     string2 = (input("Digite a segunda string composta apenas por 0 e 1: "))
     
-    validoString1 = 1 #valida para a primeira string
+    validoString1 = 1 # valida para a primeira string
     for digito in string1:
-        if digito != 0 and digito != 1:
+        if digito != '0' and digito != '1':
             validoString1 = 0
-            break #sai do for quando encontra um digito inválido
+            break # sai do for quando encontra um digito inválido
 
-    validoString2 = 1 #valida para a primeira string
+    validoString2 = 1 # valida para a segunda string
     for digito in string2:
-        if digito != 0 and digito != 1:
+        if digito != '0' and digito != '1':
             validoString2 = 0
-            break #sai do for quando encontra um digito inválido
+            break # sai do for quando encontra um digito inválido
 
-    if validoString1 == 0 or validoString2 == 0:
+    if validoString1 == 0 or validoString2 == 0: #loop continua enquanto verifica = 0
         print("As sequências devem conter apenas 0 ou 1.")
-
     elif len(string1) != len(string2):
-        print("As sequências devem ter o mesmo comprimento")
-    
+        print("As sequências devem ter o mesmo comprimento.")
     else:
-        verifica = 1
+        verifica = 1 #sai do while
 
 resultAND = ""
 resultOR = ""
@@ -38,8 +36,10 @@ for i in range(len(string1)):
     bit1 = int(string1[i])
     bit2 = int(string2[i])
 
-    resultOR += str(bit1 & bit2)
-
+    resultAND += str(bit1 & bit2)
     resultOR += str(bit1 | bit2)
-
     resultXOR += str(bit1 ^ bit2)
+
+print(f"AND: {resultAND}")
+print(f"OR: {resultOR}")
+print(f"XOR: {resultXOR}")
