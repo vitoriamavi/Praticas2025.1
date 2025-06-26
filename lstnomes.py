0b3119f69b26df71cd4ed7d98e089b010c5ae1f2
Maria Vitória <vitoriasilvaxxs@gmail.com>
2025-06-26 17:57:22 -0300

    26.06

---
+++ lstnomes.py
@@ -0,0 +1,14 @@
+import random
+
+lstNomes = ['Scooby-Doo'       , 'Fred Flintstone', 'Zé Colmeia' , 'Dom Pixote'     , 
+            'Muttley'          , 'Binicão'        , 'Tutubarão'  , 'Capitão Caverna', 
+            'Formiga Atômica'  , 'Jonny Quest'    , 'Space Ghost', 'Manda-Chuva'    , 
+            'Barney Rubble'    , 'Salsicha'       , 'Falcão Azul', 'Batatinha'      , 
+            'Penélope Charmosa', 'Pepe Legal'     , 'Catatau'    , 'Dick Vigarista' ]
+
+lstNotas = []
+for n in range(len(lstNomes)):
+    lstNotas.append([random.randint(0, 100),
+                    random.randint(0, 100),
+                    random.randint(0, 100)])
+print(lstNotas)
