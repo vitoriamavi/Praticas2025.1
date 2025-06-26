c28fc646f8c8a9d35a80eed053fcc73e86497339
vitoriamavi <vitoriasilvaxxs@gmail.com>
2025-06-26 00:37:30 -0300

    26.06

--- Lista de Charles/q5.py
+++ Lista de Charles/q5.py
@@ -1,3 +1,13 @@
 '''Questão 05 (10 pontos)
 Faça um programa que receba duas strings compostas por 0 e 1 apenas. Essas strings correspondem a uma sequência binária e devem ter o mesmo comprimento.
 Pede-se que, após a leitura e a validação das duas strings, o programa apresente o resultado das operações lógicas AND, OR e XOR entre essas duas variáveis.'''
+
+verifica = 0
+string1 = ""
+string2 = ""
+
+while verifica == 0:
+    string1 = (input("Digite a primeira string composta apenas por 0 e 1: "))
+    string2 = (input("Digite a segunda string composta apenas por 0 e 1: "))
+    valido = 1
+    
