c5d190657827d8bf5c85b548f59bdc35a654646f
vitoriamavi <vitoriasilvaxxs@gmail.com>
2025-06-28 12:34:02 -0300

    28.06

--- Lista de Charles/q7.py
+++ Lista de Charles/q7.py
@@ -2,3 +2,7 @@
 Faça um programa que simule o jogo da forca. O programa terá uma constante chamada PALAVRA_CHAVE que armazenará a palavra a ser descoberta (o programador deverá atribuir uma string ao seu 
 critério para essa constante). O programa deverá solicitar ao usuário as letras e, à medida que as letras forem sendo digitadas, o programa irá exibir se o usuário acertou ou não. 
 O jogo deverá considerar maiúsculas e minúsculas iguais. O jogador poderá errar 6 vezes antes de ser enforcado.'''
+
+palavra_chave = bolacha
+
+letra = int(input("Digite uma letra: "))
