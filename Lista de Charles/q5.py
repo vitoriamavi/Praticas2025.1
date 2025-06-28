bc880b042a8a969ed86bdc78e3730151342b51d3
vitoriamavi <vitoriasilvaxxs@gmail.com>
2025-06-28 12:31:25 -0300

    28.06

--- Lista de Charles/q5.py
+++ Lista de Charles/q5.py
@@ -2,11 +2,11 @@
 Faça um programa que receba duas strings compostas por 0 e 1 apenas. Essas strings correspondem a uma sequência binária e devem ter o mesmo comprimento.
 Pede-se que, após a leitura e a validação das duas strings, o programa apresente o resultado das operações lógicas AND, OR e XOR entre essas duas variáveis.'''
 
-verifica = 0
+verifica = False
 string1 = ""
 string2 = ""
 
-while verifica == 0:
+while not verifica:
     string1 = (input("Digite a primeira string composta apenas por 0 e 1: "))
     string2 = (input("Digite a segunda string composta apenas por 0 e 1: "))
     
@@ -27,7 +27,7 @@ while verifica == 0:
     elif len(string1) != len(string2):
         print("As sequências devem ter o mesmo comprimento.")
     else:
-        verifica = 1 #sai do while
+        verifica = True #sai do while
 
 resultAND = ""
 resultOR = ""
