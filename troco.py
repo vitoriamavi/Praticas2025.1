9cf13804a72b716ca0226fafb9a4d5ceac6f9174
vitoriamavi <vitoriasilvaxxs@gmail.com>
2025-07-01 22:28:56 -0300

    .

--- troco.py
+++ troco.py
@@ -1,2 +1,13 @@
-conta = int(input("Digite o valor da conta :"))
-pago = int(input("Digite o valor pago: "))
+print("--- Calculadora de Troco ---")
+conta = int(float(input("Valor da conta (R$): ")))
+pago = int(float(input("Valor pago (R$): ")))
+troco = pago - conta
+print(f"Troco: R$ {troco}") 
+
+if troco > 0:
+    print("\nCédulas:")
+    notas = [200, 100, 50, 20, 10, 5, 2]
+    for nota in notas:
+        quantidade = troco // nota
+        if quantidade > 0: print(f"- {quantidade} cédula(s) de R$ {nota:.0f}")
+        troco %= nota
